6911226cefe71071ce6ef48122caffdaa51f73e1
ahsansamad <ingeniouslogo@gmail.com>
2025-02-01 21:00:13 +0500

    heading-created

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var heading = document.getElementById('heading')
+var title = {
+    text: "ICC",
+    image : "assets/images/icc_logo.png"
+}
+// heading.innerHTML = title[image]
+console.log(title.image)
+heading.innerHTML = `<img class="logo" src="${title.image}"> ${title.text}`
