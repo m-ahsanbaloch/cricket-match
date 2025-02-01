36f5bae342b3a295dab50e73f73d554a139bbf25
ahsansamad <ingeniouslogo@gmail.com>
2025-02-02 01:31:57 +0500

    teams ADded

--- app.js
+++ app.js
@@ -23,10 +23,89 @@ var matches = {
         },
         season : "Test 1 of 2 (AUS leads 1-0) - Day 4- Season 2",
         date : "Jan 29-Feb-1",
-        status : "AUS won by an innings and 242 runs"
+        status : "AUS won by an innings and 242 runs",
+        play : "assets/images/team1.jpg"
     },
-   
-
+    match2 : {
+        team1 : {
+            name : "Australia",
+            australia : "654/6d",
+            image : "assets/images/austrailia.webp"
+        },
+        team2 : {
+            name : "Sri Lanka",
+            sriLanka : "165 & 247 (f/o)",
+            image : "assets/images/srilanka.png"
+        },
+        season : "Test 1 of 2 (AUS leads 1-0) - Day 4- Season 2",
+        date : "Jan 29-Feb-1",
+        status : "AUS won by an innings and 242 runs",
+        play : "assets/images/team1.jpg"
+    },
+    match3 : {
+        team1 : {
+            name : "Australia",
+            australia : "654/6d",
+            image : "assets/images/austrailia.webp"
+        },
+        team2 : {
+            name : "Sri Lanka",
+            sriLanka : "165 & 247 (f/o)",
+            image : "assets/images/srilanka.png"
+        },
+        season : "Test 1 of 2 (AUS leads 1-0) - Day 4- Season 2",
+        date : "Jan 29-Feb-1",
+        status : "AUS won by an innings and 242 runs",
+        play : "assets/images/team1.jpg"
+    },
+    match4 : {
+        team1 : {
+            name : "Australia",
+            australia : "654/6d",
+            image : "assets/images/austrailia.webp"
+        },
+        team2 : {
+            name : "Sri Lanka",
+            sriLanka : "165 & 247 (f/o)",
+            image : "assets/images/srilanka.png"
+        },
+        season : "Test 1 of 2 (AUS leads 1-0) - Day 4- Season 2",
+        date : "Jan 29-Feb-1",
+        status : "AUS won by an innings and 242 runs",
+        play : "assets/images/team1.jpg"
+    },
+    match5 : {
+        team1 : {
+            name : "Australia",
+            australia : "654/6d",
+            image : "assets/images/austrailia.webp"
+        },
+        team2 : {
+            name : "Sri Lanka",
+            sriLanka : "165 & 247 (f/o)",
+            image : "assets/images/srilanka.png"
+        },
+        season : "Test 1 of 2 (AUS leads 1-0) - Day 4- Season 2",
+        date : "Jan 29-Feb-1",
+        status : "AUS won by an innings and 242 runs",
+        play : "assets/images/team1.jpg"
+    },
+    match6 : {
+        team1 : {
+            name : "Australia",
+            australia : "654/6d",
+            image : "assets/images/austrailia.webp"
+        },
+        team2 : {
+            name : "Sri Lanka",
+            sriLanka : "165 & 247 (f/o)",
+            image : "assets/images/srilanka.png"
+        },
+        season : "Test 1 of 2 (AUS leads 1-0) - Day 4- Season 2",
+        date : "Jan 29-Feb-1",
+        status : "AUS won by an innings and 242 runs",
+        play : "assets/images/team1.jpg"
+    },
 }
 // assets/images/austrailia.webp
 console.log(inLeft)
@@ -38,10 +117,40 @@ inLeft[0].innerHTML = `<p class="status">${matches.match1.season}</p>
 console.log(inRight[0])
 inRight[0].innerHTML = `<p class="status">${matches.match1.date}</p>
 <p class="rightP">${matches.match1.team1.australia} </p>
-<p class="rightP">${matches.match1.team2.sriLanka} </p>`
-// inLeft[1].innerHTML += `<p class="status">${matches.match1.season}</p>
-// <div class="names"><img class="countryImg" src="${matches.match1.team1.image}"><p>${matches.match1.team1.name} </p> </div>
-// <div class="names"><img class="countryImg" src="${matches.match1.team2.image}"><p>${matches.match1.team2.name} </p> </div>
-// <div class="status"><p>${matches.match1.status}</p></div>`;
+<p class="rightP">${matches.match1.team2.sriLanka} </p>
+<img src="${matches.match1.play}">`
 
-// console.log(matches["match1"]["team1"])
+// ======MAtch 2============
+inLeft[1].innerHTML = `<p class="status">${matches.match2.season}</p>
+<div class="names"><img class="countryImg" src="${matches.match2.team1.image}"><p>${matches.match2.team1.name} </p> </div>
+<div class="names"><img class="countryImg" src="${matches.match2.team2.image}"><p>${matches.match2.team2.name} </p> </div>
+<div class="status"><p>${matches.match2.status}</p></div>`;
+console.log(inRight[1])
+inRight[1].innerHTML = `<p class="status">${matches.match2.date}</p>
+<p class="rightP">${matches.match2.team1.australia} </p>
+<p class="rightP">${matches.match2.team2.sriLanka} </p>
+<img src="${matches.match2.play}">`
+// ======MAtch 2============
+// ======MAtch 3============
+inLeft[2].innerHTML = `<p class="status">${matches.match3.season}</p>
+<div class="names"><img class="countryImg" src="${matches.match3.team1.image}"><p>${matches.match3.team1.name} </p> </div>
+<div class="names"><img class="countryImg" src="${matches.match3.team2.image}"><p>${matches.match3.team2.name} </p> </div>
+<div class="status"><p>${matches.match3.status}</p></div>`;
+console.log(inRight[2])
+inRight[2].innerHTML = `<p class="status">${matches.match3.date}</p>
+<p class="rightP">${matches.match3.team1.australia} </p>
+<p class="rightP">${matches.match3.team2.sriLanka} </p>
+<img src="${matches.match3.play}">`
+// ======MAtch 3============
+
+// ======MAtch 2============
+inLeft[3].innerHTML = `<p class="status">${matches.match4.season}</p>
+<div class="names"><img class="countryImg" src="${matches.match4.team1.image}"><p>${matches.match4.team1.name} </p> </div>
+<div class="names"><img class="countryImg" src="${matches.match4.team2.image}"><p>${matches.match4.team2.name} </p> </div>
+<div class="status"><p>${matches.match4.status}</p></div>`;
+console.log(inRight[3])
+inRight[3].innerHTML = `<p class="status">${matches.match4.date}</p>
+<p class="rightP">${matches.match4.team1.australia} </p>
+<p class="rightP">${matches.match4.team2.sriLanka} </p>
+<img src="${matches.match4.play}">`
+// ======MAtch 2============
